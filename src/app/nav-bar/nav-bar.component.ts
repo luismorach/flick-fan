b92ef4b052d1f9ec8eb25be2262dc6e5e1b1580f
luismorach <moraluis9825.com>
2025-09-01 10:40:47 -0500

    add new routes to menu

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,10 +1,11 @@
 import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { ComunicatorService } from '../shared/services/comunicator/comunicator.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-nav-bar',
-    imports: [NgClass],
+    imports: [NgClass,RouterLink],
     templateUrl: './nav-bar.component.html',
     styleUrl: './nav-bar.component.css'
 })
